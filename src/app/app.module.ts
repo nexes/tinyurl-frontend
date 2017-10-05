f3d305b5ec68add904b18bc22503ef5acb5bd790
Joe Berria <joeberria@gmail.com>
2017-10-04 23:50:51 -0700

    adding angular animations and material components to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import {
+    MatInputModule,
+    MdButtonModule
+} from '@angular/material';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatInputModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
