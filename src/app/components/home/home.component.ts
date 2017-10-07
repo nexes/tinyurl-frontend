8b990b6a1c4bc4095b34af0a0fbc19a1f17ebc39
Joe Berria <joeberria@gmail.com>
2017-10-06 17:34:25 -0700

    calling backend on submit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ShortenService, ShortUrl } from '../../services/shorten.service';
 
 
 @Component({
@@ -7,14 +8,36 @@ import { Component } from '@angular/core';
     styleUrls: ['home.component.css']
 })
 export class HomeComponent {
+    private shortenURL: string;
     private originalURL: string;
+    private expire: boolean;
+    private showNewUrl: boolean;
+    private expirationDate: Date;
 
-    constructor() {
+    constructor(private shortener: ShortenService) {
         this.originalURL = '';
+        this.shortenURL = '';
+        this.expire = false;
+        this.showNewUrl = false;
+        this.expirationDate = undefined;
     }
 
     submitLink() {
-        console.log('submit link', this.originalURL);
+        this.shortener.create(this.originalURL, this.expirationDate).subscribe(
+            (resp: ShortUrl) => {
+                console.log(resp);
+
+                if (resp.status === 200) {
+                    this.shortenURL = resp.url;
+                    this.showNewUrl = true;
+                }
+            },
+            (err) => {
+                console.log('creation error ', err);
+            }
+        );
+
         this.originalURL = '';
+        this.expire = false;
     }
 }
